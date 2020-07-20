4677226e56a86e16e6db0db59416fc336198ca51
corka149 <corka149@mailbox.org>
2020-07-20 21:11:08 +0200

    Added index.js

--- src/todo/app.py
+++ src/todo/app.py
@@ -1,15 +1,12 @@
 from flask import Flask
 
+from todo.views import home_views
+
 app = Flask(__name__)
-
-
-def register_blueprints():
-    from todo.views import home_views
-    app.register_blueprint(home_views.blueprint)
+app.register_blueprint(home_views.blueprint)
 
 
 def main():
-    register_blueprints()
     app.run(debug=True)
 
 
